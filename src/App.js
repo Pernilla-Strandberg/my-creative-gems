062be89adad9c50c9934ae37ca251f5c400de0bc
Pernilla-Strandberg <>
2023-12-11 11:29:39 +0000

    Feat: Add and adjust bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Bootstrap Button</Button>
+        <NavBar />
+      {/* <Button variant="primary">Bootstrap Button</Button> */}
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+          <Nav.Link>
+              <i className="fas fa-home"></i> Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i> Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i> Sign up
+            </Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
