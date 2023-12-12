c38d10217bf5dac6c3934b152b1cb599972812ed
Pernilla-Strandberg <>
2023-12-12 09:51:10 +0000

    Feat: Import and implement Route and Switch to handle menu link views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 // import Button from 'react-bootstrap/Button'
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+      <div className={styles.App}>
         <NavBar />
-      {/* <Button variant="primary">Bootstrap Button</Button> */}
-    </div>
-  );
-}
+        {/* <Button variant="primary">Bootstrap Button</Button> */}
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home page</h1>} />
+            <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+            <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
+      </div>
+    );
+  }
 
 export default App;
