da3a7028549eb1ec4bf7450e9b1b3b8536049fbf
Pernilla-Strandberg <>
2023-12-30 13:23:52 +0000

    Maint: extend login time up to 24 h by refreshing access tokens in bg by req and res interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,4 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-my-creative-gems-f8e57028547e.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // To avoid CORS error when sending cookies
-axios.defaults.withCredentials = true;  
+axios.defaults.withCredentials = true;
+// Intercept req and res to extend logged in time up to 24 h
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
