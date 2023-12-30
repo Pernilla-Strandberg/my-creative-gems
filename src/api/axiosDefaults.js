53c7d74c35076a263fba74c8f109ffec4bc4c050
Pernilla-Strandberg <>
2023-12-30 19:36:06 +0000

    Maint: Format code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-my-creative-gems-f8e57028547e.herokuapp.com/";
+axios.defaults.baseURL =
+  "https://drf-api-my-creative-gems-f8e57028547e.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // To avoid CORS error when sending cookies
 axios.defaults.withCredentials = true;
