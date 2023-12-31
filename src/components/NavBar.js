038b99e28f52c3cf3d8aaf18e5d40908ff4a5d56
Pernilla-Strandberg <>
2023-12-31 02:52:13 +0000

    Maint: correct code due to warnings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,6 @@ import {
   Navbar,
   Container,
   Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
 } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
@@ -98,7 +94,6 @@ const NavBar = () => {
       fixed="top"
     >
       <Container>
-        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
@@ -123,22 +118,6 @@ const NavBar = () => {
 
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
-          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                        </NavDropdown.Item>
-                        </NavDropdown> */}
-
-          {/* <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form> */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
